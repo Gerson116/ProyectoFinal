586417c921eccd7c12799b121c591008e580889b
gersondevx <gerson.santosm3@gmail.com>
2021-09-25 11:53:13 -0400

    Add project files.

---
+++ ProyectoFinal.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ProyectoFinal.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ ProyectoFinal.Entity/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ProyectoFinal.Entity
+{
+    public class Class1
+    {
+    }
+}
